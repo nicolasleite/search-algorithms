e3d90478b3711c7660b23136b8daca8f4cd8a8ba
Nicolas <nicolasleite96@gmail.com>
2020-06-05 11:50:13 +0000

    examples fixed + python single run

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
-python3 src/main.py ./examples/input1.in ./examples/input2.in ./examples/input3.in ./examples/input4.in ./examples/input5.in ./examples/input6.in ./examples/input7.in ./examples/input0.in
+#!/bin/bash
+
+for file in examples/*.in; do
+	python3 src/main.py $file
+done
 
--- src/main.py
+++ src/main.py
@@ -6,23 +6,20 @@ import copy
 
 def main():
 
-	sys.argv.pop(0)  # removing main.py from argv
-	for filename in sys.argv:
-		filename = str(sys.argv.pop())
-		inp = readGrid (filename)
+	filename = str(sys.argv.pop(1))
+	inp = readGrid (filename)
 
-		start = inp[1]
-		finish = inp[2]
+	start = inp[1]
+	finish = inp[2]
 
-		print ("Running Depth-First Search...")
-		grid = copy.deepcopy(inp[0])
-		depthFirst (filename, grid, start)
+	print ("Running Depth-First Search...")
+	grid = copy.deepcopy(inp[0])
+	depthFirst (filename, grid, start)
 
-		print ("Running Breadth-First Search...")
-		grid = copy.deepcopy(inp[0])
-		breadthFirst (filename, grid, start)
+	print ("Running Breadth-First Search...")
+	grid = copy.deepcopy(inp[0])
+	breadthFirst (filename, grid, start)
 
-	# analizeResults ([dfs, bfs])
 	return
 
 if __name__ == "__main__":
